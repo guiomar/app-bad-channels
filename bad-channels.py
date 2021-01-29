3d7949a3a3a5dcd48e977646b39e83ddca769c79
AuroreBussalb <aurore.bussalb@gmail.com>
2021-01-29 14:39:46 +0100

    delete the copy of raw

--- bad-channels.py
+++ bad-channels.py
@@ -24,10 +24,20 @@ calibration_file = config.pop('input_calibration')
 if calibration_file is not None:
     calibration_file = str(calibration_file)
 
+# Head pos file
+head_pos_file = config.pop('head_pos')
+if head_pos_file is not None:
+    head_pos_file = mne.chpi.read_head_pos(str(head_pos_file))
+
 # Find bad channels
+# check if line noise or cHPI signals are removed if h_freq=None
+
 raw_check = raw.copy()
 auto_noisy_chs, auto_flat_chs = mne.preprocessing.find_bad_channels_maxwell(
-    raw_check, cross_talk=cross_talk_file, calibration=calibration_file, **config['params_find_bad_channels_maxwell'])
+    raw_check, cross_talk=cross_talk_file, calibration=calibration_file, head_pos=head_pos_file,
+    **config['params_find_bad_channels_maxwell'])
+
+del raw_check
 
 bads = raw.info['bads'] + auto_noisy_chs + auto_flat_chs
 raw.info['bads'] = bads
